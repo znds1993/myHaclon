d69532f25b31c43318b498a0af735664eb864fbd
加减乘除 <lkx0830@126.com>
2021-02-20 16:41:16 +0800

    第5次

--- mainwindow.h
+++ mainwindow.h
@@ -36,8 +36,7 @@ private:
     Ui::MainWindow *ui;
     //初始化
     QAction *action1,*action2,*action3,*action4,*action6;//工具栏子项
-        bool text =false;
-        int  textt=34;
+
     void layouts();//界面
 
     void InitData();//数据
